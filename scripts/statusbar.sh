1b4c61bc9fd25b412eb47075949cf1bee4135a06
Jan Meineke <jan.meineke@mailbox.org>
2023-12-27 17:15:26 +0100

    adding statusbar script

--- scripts/statusbar.sh
+++ scripts/statusbar.sh
@@ -3,8 +3,9 @@ while true; do
     # define some default strings
     BAT=""
     MUTE=false
+    NET=""
 
-    BRIGHT=$(/run/current-system/sw/bin/light -G | sed 's/\.00//g')
+    BRIGHT=$(light -G | sed 's/\.00//g')
 
     if [[ $BRIGHT == "0" ]]; then
         BRIGHT=""
@@ -12,21 +13,27 @@ while true; do
         BRIGHT="l:$BRIGHT"
     fi
 
-    if [[ $(/run/current-system/sw/bin/amixer get Master | grep 'Left:' | awk '{ print $6 }') == '[off]' ]]; then
+    if [[ $(amixer get Master | grep 'Left:' | awk '{ print $6 }') == '[off]' ]]; then
         VOL="v:off|"
     else
 
-        VOL=$(/run/current-system/sw/bin/amixer get Master | grep 'Left:' | awk '{ print $5 }' | sed 's/\[*]*//g')
+        VOL=$(amixer get Master | grep 'Left:' | awk '{ print $5 }' | sed 's/\[*]*//g')
         VOL="v:$VOL|"
     fi
 
-    if [[ $(/run/current-system/sw/bin/acpi | /run/current-system/sw/bin/awk '!/rate/' | /run/current-system/sw/bin/awk '{ print $3 }') = 'Discharging,' ]]; then
-        BAT=$(/run/current-system/sw/bin/acpi | /run/current-system/sw/bin/awk '!/rate/' | /run/current-system/sw/bin/awk '{ print $4 }' | sed 's/\,//g') 
+    if [[ $(acpi | awk '!/rate/' | awk '{ print $3 }') = 'Discharging,' ]]; then
+        BAT=$(acpi | awk '!/rate/' | awk '{ print $4 }' | sed 's/\,//g') 
         BAT="|b:$BAT"
     fi
 
-    STATUSSTRING="[$VOL$BRIGHT$BAT] / $USER@$(hostname) \\ $(date '+%R %d.%m.%Y')"
-    /run/current-system/sw/bin/xsetroot -name "$STATUSSTRING" -d ":0"
-    sleep 1 
+    if [[ $(wpa_cli status | grep wpa_state | awk 'BEGIN { FS = "=" } ; { print $2 }' ) == 'CONNECTED' ]]; then
+        NET=$(wpa_cli status | grep ^ssid | awk 'BEGIN { FS = "=" } ; { print $2 }')
+        NET="$NET|"
+    fi
+
+
+    STATUSSTRING="[$NET$VOL$BRIGHT$BAT] / $USER@$(hostname) \\ $(date '+%R %d.%m.%Y')"
+    xsetroot -name "$STATUSSTRING" -d ":0"
+    sleep 0.5 
 done
 
